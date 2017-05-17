a79b703511bc7af47298c9a6132139647358aecb
201510995 <k90419@naver.com>
2017-05-17 13:42:33 +0900

    HI|src|test

--- src/main/java/se/smu/HI.java
+++ src/main/java/se/smu/HI.java
@@ -3,7 +3,9 @@ package se.smu;
 public class HI {
 	public static void main(String[] args){
 		System.out.println("Hello World"); //this is for testing.
-		System.out.println("Hello World");
+		System.out.println("Hello World"); //this is for testing.
+		System.out.println("Hello World"); //this is for testing.
+		System.out.println("Hello World"); //this is for testing.
 	}
 
 }
