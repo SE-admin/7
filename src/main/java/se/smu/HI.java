d4ee72915f01b220d3a1120888131243bd314f73
201511014 <ha02322@gamil.com>
2017-05-17 13:39:40 +0900

    Hi|src|연동테스트

--- src/main/java/se/smu/HI.java
+++ src/main/java/se/smu/HI.java
@@ -3,6 +3,7 @@ package se.smu;
 public class HI {
 	public static void main(String[] args){
 		System.out.println("Hello World"); //this is for testing.
+		System.out.println("Hello World");
 	}
 
 }
