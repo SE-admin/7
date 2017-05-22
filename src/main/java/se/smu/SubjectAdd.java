ffe37f3004c6bab3ca797f2c75f1aaeb5b68d359
201511014 <ha02322@gamil.com>
2017-05-22 23:41:48 +0900

    #657 |SubjectAdd |src |과목 등록 구현 완료.

--- src/main/java/se/smu/SubjectAdd.java
+++ src/main/java/se/smu/SubjectAdd.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import com.toedter.calendar.JYearChooser;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -128,28 +129,28 @@ public class SubjectAdd extends JFrame {
 		contentPane.add(ycYear);
 		
 		/*
-		 *각각 버튼 눌렀을때 색 변경 추가
+		 *각각 버튼 눌렀을때 색 변경
 		 */
 		
 		JButton btnMon = new JButton("MON");
 		btnMon.setBounds(133, 350, 65, 45);
 		contentPane.add(btnMon);
 		
-		JButton btnTUE = new JButton("TUE");
-		btnTUE.setBounds(215, 350, 65, 45);
-		contentPane.add(btnTUE);
+		JButton btnTue = new JButton("TUE");
+		btnTue.setBounds(215, 350, 65, 45);
+		contentPane.add(btnTue);
 		
-		JButton btnWED = new JButton("WED");
-		btnWED.setBounds(294, 350, 65, 45);
-		contentPane.add(btnWED);
+		JButton btnWed = new JButton("WED");
+		btnWed.setBounds(294, 350, 65, 45);
+		contentPane.add(btnWed);
 		
-		JButton btnTHU = new JButton("THU");
-		btnTHU.setBounds(374, 350, 65, 45);
-		contentPane.add(btnTHU);
+		JButton btnThu = new JButton("THU");
+		btnThu.setBounds(374, 350, 65, 45);
+		contentPane.add(btnThu);
 		
-		JButton btnFRI = new JButton("FRI");
-		btnFRI.setBounds(453, 350, 65, 45);
-		contentPane.add(btnFRI);
+		JButton btnFri = new JButton("FRI");
+		btnFri.setBounds(453, 350, 65, 45);
+		contentPane.add(btnFri);
 		
 		JButton btnSat = new JButton("SAT");
 		btnSat.setBounds(532, 350, 65, 45);
@@ -158,6 +159,41 @@ public class SubjectAdd extends JFrame {
 		JButton btnSun = new JButton("SUN");
 		btnSun.setBounds(609, 350, 65, 45);
 		contentPane.add(btnSun);
+			
+		 class SelectDay extends MouseAdapter {
+			 public void mouseClicked(MouseEvent e){
+				if(e.getClickCount() == 1){
+					btnMon.setBackground(null);
+					btnTue.setBackground(null);
+					btnWed.setBackground(null);
+					btnThu.setBackground(null);
+					btnFri.setBackground(null);
+					btnSat.setBackground(null);
+					btnSun.setBackground(null);
+					JButton btnMon = (JButton)e.getSource();
+					JButton btnTue = (JButton)e.getSource();
+					JButton btnWed = (JButton)e.getSource();
+					JButton btnThu = (JButton)e.getSource();
+					JButton btnFri = (JButton)e.getSource();
+					JButton btnSat = (JButton)e.getSource();
+					JButton btnSun = (JButton)e.getSource();
+					btnMon.setBackground(Color.CYAN);
+					btnTue.setBackground(Color.CYAN);
+					btnWed.setBackground(Color.CYAN);
+					btnThu.setBackground(Color.CYAN);
+					btnFri.setBackground(Color.CYAN);
+					btnSat.setBackground(Color.CYAN);
+					btnSun.setBackground(Color.CYAN);
+				}
+			}
+		}
+		 btnMon.addMouseListener(new SelectDay());
+		 btnTue.addMouseListener(new SelectDay());
+		 btnWed.addMouseListener(new SelectDay());
+		 btnThu.addMouseListener(new SelectDay());
+		 btnFri.addMouseListener(new SelectDay());
+		 btnSat.addMouseListener(new SelectDay());
+		 btnSun.addMouseListener(new SelectDay());
 		
 		/*
 		 * Add버튼 눌렀을때 동작.
@@ -166,14 +202,14 @@ public class SubjectAdd extends JFrame {
 		JButton btnAdd = new JButton("Add");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
 				int startHour = Integer.parseInt((String) cbStartHour.getSelectedItem());     //combobox안의 내용은 String임! int로 바꿔줘야 함
+				
 				int endHour = Integer.parseInt((String) cbEndHour.getSelectedItem());
 				String startMinute = (String) cbStartMinute.getSelectedItem();
 				String endMinute = (String) cbEndMinute.getSelectedItem();
 				String semester;
 				
-				if (cbStartAm.getSelectedItem() == "PM")   //오후시간이면 12를 더함.
+				if (cbStartAm.getSelectedItem() == "PM")
 					startHour = startHour + 12;
 				if (cbEndAm.getSelectedItem() == "PM")
 					endHour = endHour + 12;
@@ -181,7 +217,32 @@ public class SubjectAdd extends JFrame {
 					semester = "1";
 				else
 					semester = "2";
-					
+				
+				
+				String SelectDay = null;
+				if (btnMon.getBackground() == Color.CYAN)
+					SelectDay = btnMon.getText();
+				else if (btnTue.getBackground() == Color.CYAN)
+					SelectDay = btnTue.getText();
+				else if (btnWed.getBackground() == Color.CYAN)
+					SelectDay = btnWed.getText();
+				else if (btnThu.getBackground() == Color.CYAN)
+					SelectDay = btnThu.getText();
+				else if (btnFri.getBackground() == Color.CYAN)
+					SelectDay = btnFri.getText();
+				else if (btnSat.getBackground() == Color.CYAN)
+					SelectDay = btnSat.getText();
+				else if (btnSun.getBackground() == Color.CYAN)
+					SelectDay = btnSun.getText();
+				
+				
+				Subject addSubject = new Subject(txtSubject.getText(), txtProf.getText(), SelectDay, startHour, startMinute,
+						endHour, endMinute, ycYear.getValue(), semester);    //subject 구조체(?)에 내용 저장
+				
+				//DB의 Subject벡터에 꼬리 달기
+				DB.SubjectAdd(addSubject);
+				//SubjectManage의 화면 테이블에 내용 add
+				subjectmanage_parm.SubjectTable();	
 				subjectmanage_parm.setVisible(true);
 				thisSubjectAdd.dispose();
 				
