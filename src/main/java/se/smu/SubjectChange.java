d15a7afc0d9bec02e347328233d104d45a9b303d
201511014 <ha02322@gamil.com>
2017-05-23 22:13:32 +0900

    #659 |SubjectChange |src |과목변경 내용을 DataBase에 업데이트 하는 동작 구현

--- src/main/java/se/smu/SubjectChange.java
+++ src/main/java/se/smu/SubjectChange.java
@@ -209,9 +209,54 @@ public class SubjectChange extends JFrame {
 		/*
 		 * Change 버튼 눌렀을때 변경된 내용으로 새로 업데이트
 		 */
-		JButton btnNewButton = new JButton("Change");
-		btnNewButton.setBounds(595, 38, 105, 45);
-		contentPane.add(btnNewButton);
+		 JButton btnNewButton = new JButton("Change");
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					int startHour = Integer.parseInt((String) cbStartHour.getSelectedItem());     //combobox안의 내용은 String임! int로 바꿔줘야 함
+					int endHour = Integer.parseInt((String) cbEndHour.getSelectedItem());
+					String startMinute = (String) cbStartMinute.getSelectedItem();
+					String endMinute = (String) cbEndMinute.getSelectedItem();
+					String semester;
+					
+					if (cbStartAm.getSelectedItem() == "PM")
+						startHour = startHour + 12;
+					if (cbEndAm.getSelectedItem() == "PM")
+						endHour = endHour + 12;
+					if (cbSemester.getSelectedItem() == "1")
+						semester = "1";
+					else
+						semester = "2";
+					
+					
+					String SelectDay = null;
+					if (btnMon.getBackground() == Color.CYAN)
+						SelectDay = btnMon.getText();
+					else if (btnTue.getBackground() == Color.CYAN)
+						SelectDay = btnTue.getText();
+					else if (btnWed.getBackground() == Color.CYAN)
+						SelectDay = btnWed.getText();
+					else if (btnThu.getBackground() == Color.CYAN)
+						SelectDay = btnThu.getText();
+					else if (btnFri.getBackground() == Color.CYAN)
+						SelectDay = btnFri.getText();
+					else if (btnSat.getBackground() == Color.CYAN)
+						SelectDay = btnSat.getText();
+					else if (btnSun.getBackground() == Color.CYAN)
+						SelectDay = btnSun.getText();
+					
+					Subject addSubject = new Subject(txtSubject.getText(), txtProf.getText(), SelectDay, startHour, startMinute,
+							endHour, endMinute, ycYear.getValue(), semester);    //subject에 내용 저장
+					
+					//SubjectManage의 화면 테이블에 내용 add
+					DB.getSelectedSubject(selectedRow);
+					DB.SubjectChange(addSubject, selectedRow);	
+					subjectmanage_parm.SubjectTable();	
+					subjectmanage_parm.setVisible(true);
+					thisSubjectChange.dispose();
+				}
+			});
+			btnNewButton.setBounds(595, 38, 105, 45);
+			contentPane.add(btnNewButton);
 	}
 
 }
